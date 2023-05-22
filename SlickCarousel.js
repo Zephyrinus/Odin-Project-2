28c69da6b5ad9c1fe899711224a6c0b02cf07d28
SCZephyrinus <nicolas22443@gmail.com>
2023-05-22 16:51:27 -0500

    commit

--- SlickCarousel.js
+++ SlickCarousel.js
@@ -1,10 +1,49 @@
-$(document).ready(function(){
-    $('.step2boxes').slick({
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 1
-    });
-    
+const scrollItems = document.querySelector('.scroll-items');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
 
-  });
-  
+let scrollAmount = 0;
+let scrollMax = scrollItems.scrollWidth - scrollItems.clientWidth;
+
+function scrollLeft() {
+  if (scrollAmount > 0) {
+    scrollAmount -= scrollItems.clientWidth;
+    scrollItems.style.transform = `translateX(-${scrollAmount}px)`;
+    updateButtons();
+  } else {
+    scrollAmount = scrollMax;
+    scrollItems.style.transform = `translateX(-${scrollAmount}px)`;
+    updateButtons();
+  }
+}
+
+function scrollRight() {
+  if (scrollAmount < scrollMax) {
+    scrollAmount += scrollItems.clientWidth;
+    scrollItems.style.transform = `translateX(-${scrollAmount}px)`;
+    updateButtons();
+  } else {
+    scrollAmount = 0;
+    scrollItems.style.transform = `translateX(-${scrollAmount}px)`;
+    updateButtons();
+  }
+}
+
+function updateButtons() {
+  if (scrollAmount === 0) {
+    prevButton.classList.add('disabled');
+  } else {
+    prevButton.classList.remove('disabled');
+  }
+
+  if (scrollAmount === scrollMax) {
+    nextButton.classList.add('disabled');
+  } else {
+    nextButton.classList.remove('disabled');
+  }
+}
+
+prevButton.addEventListener('click', scrollLeft);
+nextButton.addEventListener('click', scrollRight);
+
+updateButtons();
